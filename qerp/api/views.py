96148b28762b3797c8cd56e4aa36c2d0b7679b1f
bryanmayodi <bryanmayodi@gmail.com>
2024-01-08 12:40:18 +0300

    finished creating views for the api endpoints

--- qerp/api/views.py
+++ qerp/api/views.py
@@ -7,3 +7,54 @@ from .serializers import EmployeeSerializer, InventorySerializer, ProjectSeriali
 
 # Create your views here.
 
+class EmployeeList(generics.ListCreateAPIView):
+    serializer_class = EmployeeSerializer
+
+    def get_queryset(self):
+        queryset = Employee.objects.all()
+
+        return queryset
+    
+class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = EmployeeSerializer
+    queryset = Employee.objects.all()
+
+class InventoryList(generics.ListCreateAPIView):
+    serializer_class = InventorySerializer
+
+    def get_queryset(self):
+        queryset = Inventory.objects.all()
+
+        return queryset
+    
+class InventoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = InventorySerializer
+    queryset = Inventory.objects.all()
+
+class ProjectList(generics.ListCreateAPIView):
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        queryset = Project.objects.all()
+
+        return queryset
+    
+class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
+
+class CustomerList(generics.ListCreateAPIView):
+    serializer_class = CustomerSerializer
+
+    def get_queryset(self):
+        queryset = Customer.objects.all()
+
+        return queryset
+    
+class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CustomerSerializer
+
+    def get_queryset(self):
+        queryset = Customer.objects.all()
+
+        return queryset
